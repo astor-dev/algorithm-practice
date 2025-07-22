35e700bdc6ff45b57b2f8505f38c10cfc74eb39a
astor-dev <orangnlp@gmail.com>
2025-07-22 16:07:18 +0900

    나무자르기

--- baekjoon/2805_나무_자르기.java
+++
@@ -1,6 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    }
-}
--- baekjoon/이분 탐색(binary search)/2805_나무_자르기.java
+++ baekjoon/이분 탐색(binary search)/2805_나무_자르기.java
@@ -1,4 +1,3 @@
-package playground;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
