7201da84053fb475073c9cc3e6c1862073de19fd
SDS <orangnlp@gmail.com>
2025-08-04 13:45:34 +0900

    fix: gradle 제거

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+    
+    
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
