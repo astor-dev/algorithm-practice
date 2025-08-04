29e0268556fb57a195029e40f3fe4e2ce9b0acd6
astor <119511836+astor-dev@users.noreply.github.com>
2025-08-04 11:44:00 +0900

    Update 1753_최단경로(dijkstra).java

--- baekjoon/그래프와 순회(graph traversal)/1753_최단경로(dijkstra).java
+++ baekjoon/그래프와 순회(graph traversal)/1753_최단경로(dijkstra).java
@@ -1,89 +1,62 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.*;
 
-public class Main{ 
 
-    static ArrayList<Node>[] graph;
-    static int[] d;
-    static int N, M, start;
-    static final int INF = Integer.MAX_VALUE;
-    static PriorityQueue<Node>pq = new PriorityQueue<>();
+public class Main {
+    static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer stringTokenizer;
+    static StringBuilder stringBuilder = new StringBuilder();
 
-    public static void main(String[] args) throws Exception {
-        try{   
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken());
-
-            start = Integer.parseInt(br.readLine());
-            graph = new ArrayList[N+1];
-            for(int i = 0; i<N+1; i++){
-                graph[i] = new ArrayList<Node>();
-            }
-            for(int i = 0; i<M; i++){
-                st = new StringTokenizer(br.readLine());
-                int startNode = Integer.parseInt(st.nextToken());
-                int endNode = Integer.parseInt(st.nextToken());
-                int weight = Integer.parseInt(st.nextToken());            
-                graph[startNode].add(new Node(endNode, weight));         
-            }
-            d = new int[N+1];
-            Arrays.fill(d, INF);
-            dijkstra();        
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace();            
+    static class Edge {
+        int target;
+        int weight;
+        Edge(int target, int weight) {
+            this.target = target;
+            this.weight =weight;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int v = Integer.parseInt(stringTokenizer.nextToken()), e = Integer.parseInt(stringTokenizer.nextToken());
+        int startNode = Integer.parseInt(bufferedReader.readLine());
+        List<List<Edge>> adjEdges = new ArrayList<>();
+        for (int i = 0; i <= v; i++) {
+            adjEdges.add(new ArrayList<>());
         }
 
-    }  
-    static void dijkstra(){
-        
-        graph[start].add(new Node(start, 0));
-        pq.offer(new Node(start, 0));
-        
-        while(!pq.isEmpty()){
-            Node n = pq.poll();
-            if(n.getDistance() > d[n.getNode()]) continue;
-            for(Node tempNode : graph[n.getNode()]){
-                int tempDistance = n.getDistance() + tempNode.getDistance();
-                if(tempDistance < d[tempNode.getNode()]){
-                    d[tempNode.getNode()] = tempDistance;
-                    pq.offer(new Node(tempNode.getNode() ,tempDistance));
+
+        for (int i = 1; i <= e; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int from = Integer.parseInt(stringTokenizer.nextToken());
+            int to = Integer.parseInt(stringTokenizer.nextToken());
+            int weight = Integer.parseInt(stringTokenizer.nextToken());
+            adjEdges.get(from).add(new Edge(to, weight));
+        }
+
+        PriorityQueue<Edge> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(edge -> edge.weight));
+        int[] distance = new int[v+1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        priorityQueue.add(new Edge(startNode, 0));
+        distance[startNode] = 0;
+        while(!priorityQueue.isEmpty()) {
+            Edge current = priorityQueue.poll();
+            if(distance[current.target] < current.weight) continue;
+            for( Edge adj : adjEdges.get(current.target) ) {
+                int adjNewCost = distance[current.target] + adj.weight;
+                if(adjNewCost < distance[adj.target]) {
+                    priorityQueue.add(new Edge(adj.target, adjNewCost));
+                    distance[adj.target] = adjNewCost;
                 }
             }
         }
-        for(int i = 1; i<d.length; i++){
-            if(d[i] == INF){
-                System.out.println("INF");
-            } else{
-                System.out.println(d[i]);
+        for (int i = 1; i <= v; i++) {
+            if(distance[i] == Integer.MAX_VALUE) {
+                stringBuilder.append("INF").append("\n");
+                continue;
             }
+            stringBuilder.append(distance[i]).append("\n");
         }
-    }
-    
-}
-class Node implements Comparable<Node>{
-    private int node;
-    private int distance;
-    
-    Node(int node, int distance){
-        this.node = node;
-        this.distance = distance;
-    }
-    public int getNode(){
-        return node;
-    }
-    
-    public int getDistance(){
-        return distance;
-    }
-    @Override
-    public int compareTo(Node target){
-        return (this.distance > target.distance)
-        ? 1
-        : -1;
+
+        System.out.println(stringBuilder);
     }
 }
